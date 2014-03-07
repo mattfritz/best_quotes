7f5c8cffcce7a0467bee96f553545a569411bc4d
Matt Fritz <mfritz@itriagehealth.com>
2014-03-07 11:12:53 -0700

    add env printout to QuotesController to see what Rack feeds Rails

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,5 +1,6 @@
 class QuotesController < Rulers::Controller
   def a_quote
-    "There is nothing either good or bad but thinnking makes it so."
+    "There is nothing either good or bad but thinking makes it so." +
+      "\n<pre>\n#{env}\n</pre>"
   end
 end
