5111c968565ec923edd8f6d694f00216c37bee65
Matt Fritz <mfritz@itriagehealth.com>
2014-03-07 13:26:29 -0700

    add exception action to test error messaging

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -3,4 +3,8 @@ class QuotesController < Rulers::Controller
     "There is nothing either good or bad but thinking makes it so." +
       "\n<pre>\n#{env}\n</pre>"
   end
+
+  def exception
+    raise "It's a bad one!"
+  end
 end
