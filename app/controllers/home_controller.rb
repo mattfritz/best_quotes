1cdd281605028fab57686f5a212d17f34ac23e41
Matt Fritz <mfritz@itriagehealth.com>
2014-03-10 10:38:43 -0600

    added and required HomeController to test default controller routes

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,5 @@
+class HomeController < Rulers::Controller
+  def index
+    "This is the INDEX action of the HOME controller."
+  end
+end
--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require "rulers"
 $LOAD_PATH << File.join(File.dirname(__FILE__), "..", "app", "controllers")
 require "quotes_controller"
+require "home_controller"
 
 module BestQuotes
   class Application < Rulers::Application
