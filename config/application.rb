3395efc276a1730600e20f92ca1f5fdb682e6ff4
Matt Fritz <mfritz@itriagehealth.com>
2014-03-11 12:41:46 -0600

    modification to remove duplicate requirements

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,6 @@
 require "rulers"
+
 $LOAD_PATH << File.join(File.dirname(__FILE__), "..", "app", "controllers")
-require "quotes_controller"
-require "home_controller"
 
 module BestQuotes
   class Application < Rulers::Application
